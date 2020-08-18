fd971205fe4264706f972f7f4efc20dc50e47762
sean-slaughter <sthompson2822@gmail.com>
2020-08-18 14:33:26 -0500

    created user model

---
+++ db/migrate/20200818193303_create_coffeeshops.rb
@@ -0,0 +1,14 @@
+class CreateCoffeeshops < ActiveRecord::Migration[6.0]
+  def change
+    create_table :coffeeshops do |t|
+      t.string :name
+      t.string :address
+      t.float :rating
+      t.string :yelp_url
+      t.string :image_url
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
