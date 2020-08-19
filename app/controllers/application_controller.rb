aa73230c78b3feb57326a8e786f76e7da38f35d3
sean-slaughter <sthompson2822@gmail.com>
2020-08-19 12:59:07 -0500

    finish registration/login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,11 @@
 class ApplicationController < ActionController::Base
-
+    helper_method :current_user, :logged_in?
+    
     def current_user
         session[:user_id] ? User.find(session[:user_id]) : nil
     end
 
     def logged_in?
         !!current_user
-    end
-
-    def require_login
-        redirect_to login_path unless logged_in?
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        if @user.save?
+        if @user.valid?
+            @user.save
             session[:user_id] = @user.id
             redirect_to @user
         else
@@ -23,6 +24,9 @@ class UsersController < ApplicationController
     
     def user_params
         params.require(:user).permit(:name, :location, :email, :password, :password_confirmation)
+    end
+    def require_login
+        redirect_to login_path unless logged_in?
     end
 
 end
