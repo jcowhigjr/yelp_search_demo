59644493c4ebc25642ca3ecd12ffa0f8c34a255b
sean-slaughter <sthompson2822@gmail.com>
2020-08-19 13:20:16 -0500

    render _coffeeshop in index

--- app/controllers/coffeeshops_controller.rb
+++ app/controllers/coffeeshops_controller.rb
@@ -2,6 +2,7 @@ class CoffeeshopsController < ApplicationController
   def index
     if params[:query]
       @coffeeshops = Coffeeshop.get_search_results(params[:query])
+      @search = params[:query]
     else
       @coffeeshops = Coffeeshop.all
     end
