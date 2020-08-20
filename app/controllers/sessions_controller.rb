8626f4b1b6839a8fc6a7201e3efed8823e6add9f
sean-slaughter <sthompson2822@gmail.com>
2020-08-20 10:58:28 -0500

    add route and controller action for omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,4 +16,8 @@ class SessionsController < ApplicationController
     session.clear
     redirect_to root_path
   end
+
+  def create_from_google
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   end
 
   get '/login', to: 'sessions#new', as: 'login'
+  get '/auth/google_oauth2/callback', to: "sessions#create_with_google"
   post 'sessions/', to: 'session#create', as: 'sessions'
   delete 'sessions/', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
