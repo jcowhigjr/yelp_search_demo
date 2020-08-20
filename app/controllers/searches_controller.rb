b163ab4ba8b934c9ee194ad5f790a5f780c2432e
sean-slaughter <sthompson2822@gmail.com>
2020-08-20 13:02:05 -0500

    create searches as model

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,10 +1,22 @@
 class SearchesController < ApplicationController
 
   def create
+    @search = determine_search_type
+    Coffeeshop.get_search_results(params[:query]).each{|i| @search.coffeeshops << i}
+    if @search.save
+      redirect_to @search
+    else
+      redirect_to root_path, error: "Something went wrong with your search please try again."
+    end
   end
 
   def show
   end
 
+  private
+
+  def determine_search_type
+    logged_in? ? Search.create(query: params[:query]), user: current_user) : Search.create(query: params[:query])
+  end
 
 end
--- db/migrate/20200820171414_create_searches.rb
+++ db/migrate/20200820171414_create_searches.rb
@@ -1,7 +1,7 @@
 class CreateSearches < ActiveRecord::Migration[6.0]
   def change
     create_table :searches do |t|
-      t.belongs_to :user
+      t.belongs_to :user, null: true
       t.string :query
 
       t.timestamps
