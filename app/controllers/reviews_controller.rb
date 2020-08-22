a38094a5806c4546f877981ea0775b5b8813317f
sean-slaughter <sthompson2822@gmail.com>
2020-08-21 21:49:22 -0500

    add user_id to review strong params

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+
+    def create
+        @coffeehop = Coffeeshop.find(params[:coffeeshop_id])
+        @coffeeshop.reviews.build(review_params)
+        if @coffeshop.save
+            redirect_to @coffeshop
+        else
+            flash[:error] = "Something went wrong creating review"
+            redirect_to @coffeeshop
+        end
+    end
+
+private
+
+    def review_params
+        params.require(:review).permit(:content, :rating, :user_id)
+    end
 end
