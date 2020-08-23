efa5dbea96b92d888d303b82f0e4b6e0788d1d42
sean-slaughter <sthompson2822@gmail.com>
2020-08-23 11:21:18 -0500

    add user_id to session at login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
       redirect_to user
     else
       flash[:error] =  "Your email or password do not match our records."
