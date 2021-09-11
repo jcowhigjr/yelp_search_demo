184f4a6edc97ca09c9b784b3a417dcb26948bd4e
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2021-09-11 17:01:11 -0400

    fix logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,20 @@
 Rails.application.routes.draw do
-  
-  
-  root to: "static#home"
-  resources :searches, only: [:new, :create, :show]
+  root to: 'static#home'
+  resources :searches, only: %i[new create show]
 
   resources :users do
-    resources :reviews, only: [:edit, :update, :destroy]
+    resources :reviews, only: %i[edit update destroy]
   end
 
-  resources :user_favorites, only: [:create, :destroy]
+  resources :user_favorites, only: %i[create destroy]
 
   resources :coffeeshops, only: [:show] do
-    resources :reviews, only: [ :new, :create]
+    resources :reviews, only: %i[new create]
   end
 
   get '/login', to: 'sessions#new', as: 'login'
-  get '/auth/google_oauth2/callback', to: "sessions#create_with_google"
+  get '/auth/google_oauth2/callback', to: 'sessions#create_with_google'
   post '/sessions', to: 'sessions#create', as: 'sessions'
-  delete '/sessions', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
