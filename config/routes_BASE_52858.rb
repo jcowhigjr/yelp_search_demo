1eb231ff80318552692a29784b827d7cf5871f4d
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2021-09-16 13:06:24 -0400

    wip

---
+++ config/routes_BASE_52858.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  
+  
+  root to: "static#home"
+  resources :searches, only: [:new, :create, :show]
+
+  resources :users do
+    resources :reviews, only: [:edit, :update, :destroy]
+  end
+
+  resources :user_favorites, only: [:create, :destroy]
+
+  resources :coffeeshops, only: [:show] do
+    resources :reviews, only: [ :new, :create]
+  end
+
+  get '/login', to: 'sessions#new', as: 'login'
+  get '/auth/google_oauth2/callback', to: "sessions#create_with_google"
+  post '/sessions', to: 'sessions#create', as: 'sessions'
+  delete '/sessions', to: 'sessions#destroy'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
