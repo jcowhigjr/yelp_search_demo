9ca272f60628fe7386924770ecab148bac49bf57
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2021-09-18 15:41:49 -0400

    ./bin/rails importmap:install;   bin/rails  turbo:install

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,1 @@
-// Entry point for the build script in your package.json
-import "./controllers"
-import "@hotwired/turbo-rails"
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
