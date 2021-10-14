ca57f393be911b715811c05b51c595d3b8ddb816
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2021-10-14 13:18:55 -0400

    resolves a deprecation warning for legacy connection handling when running bin/rails test

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Jitter
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.autoloader = :zeitwerk
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,5 +19,8 @@ module Jitter
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    #Read more about how to migrate at: https://guides.rubyonrails.org/active_record_multiple_databases.html#migrate-to-the-new-connection-handling
+    config.active_record.legacy_connection_handling = false
   end
 end
