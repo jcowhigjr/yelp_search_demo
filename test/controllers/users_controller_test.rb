065fe2b55910fd838e00c6ede525dec0faa20304
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2021-10-14 15:34:56 -0400

    Fixed controller tests which are actually integration tests which were failing due to routing and routing helper mis configuration

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @user = users(:one)
+    @user_favorite = user_favorites(:one)
+    @user.name = 'John'
+    # allow_any_instance_of(ActionDispatch::SystemTestCase).to receive(:current_user).and_return(users(:one))
+    # @controller.stubs(:current_user).returns(users(:one))
+    get '/login'
+  end
+
+  test "the truth" do
+    assert true
+  end
 end
