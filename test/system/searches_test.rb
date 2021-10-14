335c06c780527d3046d0def1c66f5c5389638126
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2021-10-14 16:45:00 -0400

    Added System Test  'An anonymous user at the static home can search by zip to return a list of coffeeshops from yelp api' and credentials

--- test/system/searches_test.rb
+++ test/system/searches_test.rb
@@ -1,9 +1,12 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class SearchesTest < ApplicationSystemTestCase
-  test "visiting the index" do
-    visit searches_url
-  
-    assert_selector "h1", text: "Search"
+  test 'Static Home Has a Search field' do
+    visit static_home_url
+    fill_in('query', :with => '30312')
+    click_button('Search')
+    #TODO: create a mock response from Yelp.
+    page.should have_selector('.search-results > .row.center > .white-text')
+    page.should have_content('Bellwood')
   end
 end
