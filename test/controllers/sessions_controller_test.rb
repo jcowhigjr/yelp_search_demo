65430fc4856bf5e1a2139a2674bdd4fab2f9e304
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2021-10-20 13:47:16 -0400

    Fixed the sessions controller tests with correct flash message, status code, and session_params structure   This illustrates that the params hash for creating a session does not match the params hash for creating and updating a user done via the users controller because it is not nested as a user hash

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,19 +1,32 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get login" do
+  test 'should get login' do
     get login_url
     assert_response :success
   end
-#TODO: move tests for session
-  # test "should get create" do
-  #   get sessions_create_url
-  #   assert_response :success
-  # end
 
-  # test "should get destroy" do
-  #   get sessions_destroy_url
-  #   assert_response :success
-  # end
+  setup do
+    @user = users(:two)
+    @user.password = 'mypass'
+    @user.password_confirmation = 'mypass'
+    @user.save!
+  end
 
+  test 'should sign in user' do
+    post sessions_path, params: {
+      email: @user.email, password: 'mypass'
+    }
+    assert_response :ok
+    assert_equal 'Logged in!', flash[:success]
+  end
+
+  test 'should redirect to login on fail' do
+    post sessions_path, params: {
+      email: @user.email, password: 'badd password'
+    }
+    assert_response :found
+    assert_equal 'Your email or password do not match our records.', flash[:error]
+    assert_redirected_to login_path
+  end
 end
