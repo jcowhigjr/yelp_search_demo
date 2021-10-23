173ddb4f4d2837c84c22d00f40f52fd8e986291b
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2021-10-23 16:12:31 -0400

    Added System Test coverage for favoriting and adding reviews rating and text to coffeeshops

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,23 +1,22 @@
 class Review < ApplicationRecord
-    validates :content, :rating, :user, :coffeeshop, presence: true
-    validates :rating, inclusion: {in: 1..5}
-    belongs_to :user
-    belongs_to :coffeeshop
-    scope :order_reviews, -> (user_id) {where('user_id == ?', user_id).order('rating desc')}
+  validates :content, :rating, :user, :coffeeshop, presence: true
+  validates :rating, inclusion: { in: 1..5 }
+  belongs_to :user
+  belongs_to :coffeeshop
+  scope :order_reviews, ->(user_id) { where('user_id == ?', user_id).order('rating desc') }
 
-
-    def rating_in_stars
-        case self.rating
-        when 1
-            "★☆☆☆☆"
-        when 2
-            "★★☆☆☆"
-        when 3
-            "★★★☆☆"
-        when 4
-            "★★★★☆"
-        when 5
-            "★★★★★"
-        end
+  def rating_in_stars
+    case rating
+    when 1
+      '★☆☆☆☆'
+    when 2
+      '★★☆☆☆'
+    when 3
+      '★★★☆☆'
+    when 4
+      '★★★★☆'
+    when 5
+      '★★★★★'
     end
+  end
 end
