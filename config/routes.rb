63231049d995cff4f0db6f5bfd60739a2639b72b
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2021-10-23 17:14:46 -0400

    Fixed the broken login with google button. Added System Test to start the login but is limited since the automated browser is not considered secure.

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   end
 
   get '/login', to: 'sessions#new', as: 'login'
-  get '/auth/google_oauth2/callback', to: 'sessions#create_with_google'
+  get '/auth/google_oauth2/callback', to: 'sessions#create_with_google', as: 'google_login'
   post '/sessions', to: 'sessions#create', as: 'sessions'
   get '/logout', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
