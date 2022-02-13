072596c139038f071ba1c6c119cca635767a6f12
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2022-02-12 22:21:42 -0500

    Moved the add and remove favorite into a turbo frame and updated the integration tests to spec this behavior and the redirect to the coffeeshop using a turboframe.

--- app/controllers/user_favorites_controller.rb
+++ app/controllers/user_favorites_controller.rb
@@ -2,21 +2,23 @@ class UserFavoritesController < ApplicationController
   def create
     user_fav = current_user.user_favorites.build(coffeeshop_id: params[:coffeeshop_id])
     if user_fav.save
-      flash[:success] = "Coffeeshop is added to your favorites."
-      redirect_to current_user
+      flash[:success] = 'Coffeeshop is added to your favorites.'
+      @coffeeshop = user_fav.coffeeshop
+      redirect_to @coffeeshop
     else
-      flash[:error] = "Something went wrong when adding to your favorites."
+      flash[:error] = 'Something went wrong when adding to your favorites.'
       redirect_to current_user
     end
   end
 
   def destroy
     set_user_favorite
+    @coffeeshop = @user_favorite.coffeeshop
     if @user_favorite.destroy
-      flash[:success] = "Coffeeshop is removed from your favorites."
-      redirect_to current_user
+      flash[:success] = 'Coffeeshop is removed from your favorites.'
+      redirect_to @coffeeshop
     else
-      flash[:error] = "Something went wrong when removing your favorite."
+      flash[:error] = 'Something went wrong when removing your favorite.'
       redirect_to current_user
     end
   end
