ae665c6dabf7822084d7c98b3d6d30e8681a9b82
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2022-02-28 16:13:12 -0500

    Added geolocation target fields for latitude and longitude into search model database. Added geolocation stimulus controller and view form Added longitude and latitude to yelp search query Added system test case sleep to allow browser to query the geolocation Changed the search query to be for the type of food rather than the location

--- test/system/searches_test.rb
+++ test/system/searches_test.rb
@@ -1,11 +1,19 @@
 require 'application_system_test_case'
 
 class SearchesTest < ApplicationSystemTestCase
+  focus
   test 'An anonymous user at the static home can search by zip to return a list of coffeeshops from yelp api' do
     visit static_home_url
-    fill_in('query', with: '30312')
+    fill_in('query', with: 'seafood')
+    assert_selector(:field, 'query', with: 'seafood')
+    sleep 4
+    # assert_selector(:field, 'latitude', with: '36.91')
+    # assert_selector(:field, 'longitude', with: '-79.99')
+
     click_button('Search')
+
     click_on('More Info', match: :first)
+
     assert_current_path %r{^/coffeeshops/\d{1,9}}
   end
 end
