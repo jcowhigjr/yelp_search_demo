c1cbc323078b6c3e9836cbade1121f70746ac1db
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2022-03-10 16:12:09 -0500

    redirect search controller spec started failing so clarified routes and updated the controller to use the create route behavior searches controller spec passing now

--- test/system/broadcasts_test.rb
+++ test/system/broadcasts_test.rb
@@ -2,39 +2,7 @@ require 'application_system_test_case'
 
 class BroadcastsTest < ApplicationSystemTestCase
   setup { Coffeeshop.delete_all }
-  # setup do
-  #   @coffeeshop_one = coffeeshops(:one)
-  # end
-  # test 'Coffeeshop broadcasts Turbo Streams' do
-  #   visit static_home_url
-  #   fill_in('query', with: '30312')
-  #   click_button('Search')
-  #   @coffeeshop = Coffeeshop.first
-  #   assert_text 'Coffeeshops'
-  #   assert_broadcasts_text 'Coffeeshop Broadcast Test' do |text|
-  #     @coffeeshop.update(name: text).broadcast_append_to(:coffeeshops)
-  #   end
-  # end
 
-  # test 'New coffeeshops update the coffeeshop count with html' do
-  #   visit static_home_url
-  #   fill_in('query', with: '30312')
-  #   click_button('Search')
-  #   assert_text 'Coffeeshops'
-  #   assert_difference "Coffeeshop.count", 1 do
-  #     coffeeshop = Coffeeshop.create(name: 'A new coffeeshop')
-  #     coffeeshop.broadcast_append_to(:coffeeshops)
-  #   end
-  # end
-
-  # test "Users::Profile broadcasts Turbo Streams" do
-  #   visit users_profiles_path
-
-  #   assert_text "Users::Profiles"
-  #   assert_broadcasts_text "Profile 1" do |text|
-  #     Users::Profile.new(id: 1, name: text).broadcast_append_to(:users_profiles)
-  #   end
-  # end
 
   private
 
