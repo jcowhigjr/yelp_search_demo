c86ff549bf5c79b6f5bf663226d9dd42fcaf1f3f
jcowhigjr <1895349+jcowhigjr@users.noreply.github.com>
2022-03-10 21:50:31 -0500

    attack the slow search problem by starting with a default location and enable the tests to use that for headless testing in CI (#2)
    * attack the slow search problem by starting with a default location and enable the tests to use that for headless testing in CI
    
    set default to 0.0,0.0
    updated system tests to run and look for this value in the UI
    be explicit with validation at the model level
    update the user text to make it clear submitting a search will use their device location
    
    * redirect search controller spec started failing so clarified routes and updated the controller to use the create route behavior
    searches controller spec passing now
    
    * Fixing failing test in github CI
    
    Capybara::Ambiguous: Ambiguous match, found 2 elements matching visible css #review_rating
        test/system/coffeeshops_test.rb:40:in
    
    * fixed github ci system tests to test without true geo search that i haven't been able to enable with the headless cuprite driver
    
    should probably unify on HEADLESS and SHOW_TESTS but for now this should clarify we want to search using the default 0.0 geo cordinates when the capabilities aren't possible
    
     Please enter the commit message for your changes. Lines starting

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,16 +1,21 @@
 class SearchesController < ApplicationController
   def new
-    @search = Search.create
+    redirect_to Search.create
+  end
+
+  def update
+    @search = Search.find(params[:id])
+    @search.update(search_params)
     redirect_to @search
   end
 
   def create
     @search = determine_search_type
-    if Coffeeshop.get_search_results(params[:query], @search) == 'error'
+    if Coffeeshop.get_search_results(@search) == 'error'
       flash[:error] = 'Something went wrong with your search, please try again.'
       redirect_to static_home_url
     else
-      @search.save
+      @search.save!
       redirect_to_proper_path
     end
   end
@@ -21,14 +26,15 @@ class SearchesController < ApplicationController
 
   private
 
+  def search_params
+    params.permit(:search, :query, :latitude, :longitude)
+  end
+
   def determine_search_type
     if logged_in?
-      Search.create(query: params[:query], latitude: params[:latitude], longitude: params[:longitude],
-                    user: current_user)
+      Search.new(search_params.merge(user: current_user))
     else
-      Search.create(
-        latitude: params[:latitude], longitude: params[:longitude], query: params[:query]
-      )
+      Search.new(search_params)
     end
   end
 
@@ -36,7 +42,7 @@ class SearchesController < ApplicationController
     if logged_in?
       redirect_to @search
     else
-      render 'static/home', locals: { search: @search }
+      render 'static/home', locals: { search: @search }, notice: 'Search created, please login to save your search.'
     end
   end
 end
--- test/models/search_test.rb
+++ test/models/search_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
+require 'minitest/autorun'
 
 class SearchTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test 'should not save without a location' do
+    search = Search.new
+    assert_not search.save
+  end
+
+  test 'should not save without a query' do
+    search = Search.new
+    assert_not search.save
+  end
+
+  test 'should not save without a user' do
+    search = Search.new
+    assert_not search.save
+  end
+
+  test 'should save with a location and query' do
+    search = Search.new
+    search.query = 'yoga'
+    assert(search.latitude === 0.0)
+    assert(search.longitude === 0.0)
+    assert search.save
+  end
+
 end
