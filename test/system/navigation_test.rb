2ca79c9d77f32239dc13af1c5438d63fb2f7ddb6
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2022-03-18 16:01:08 -0400

    removed the duplicated search testing since the default 0.0 San Fran work well now in the search system test

--- test/system/navigation_test.rb
+++ test/system/navigation_test.rb
@@ -9,19 +9,6 @@ class NavigationTest < ApplicationSystemTestCase
     visit new_search_path
     fill_in 'search[query]', with: 'tacos'
 
-    if ENV['SHOW_TESTS'] && !ENV['CUPRITE']
-      # sleeping for a second to allow the geolocation api call to complete
-      sleep 3
-      # need to stub the geolocation api call default is 0.0
-      assert_no_selector(:field, 'latitude', type: 'hidden', with: '0.0')
-      assert_no_selector(:field, 'longitude', type: 'hidden', with: '0.0')
-
-    else
-      # use default geolocation values
-      assert_selector(:field, 'latitude', type: 'hidden', with: '0.0')
-      assert_selector(:field, 'longitude', type: 'hidden', with: '0.0')
-    end
-
     click_button 'Search'
 
     assert_current_path search_path(Search.last)
--- test/system/sessions/logout_test.rb
+++ test/system/sessions/logout_test.rb
@@ -24,18 +24,7 @@ class LogoutTest < ApplicationSystemTestCase
     click_link 'Search'
     click_on 'Logout'
     fill_in 'search[query]', with: 'tacos'
-    if ENV['SHOW_TESTS'] && !ENV['CUPRITE']
-      # sleeping for a second to allow the geolocation api call to complete
-      sleep 3
-      # need to stub the geolocation api call default is 0.0
-      assert_no_selector(:field, 'latitude', type: 'hidden', with: '0.0')
-      assert_no_selector(:field, 'longitude', type: 'hidden', with: '0.0')
 
-    else
-      # use default geolocation values
-      assert_selector(:field, 'latitude', type: 'hidden', with: '0.0')
-      assert_selector(:field, 'longitude', type: 'hidden', with: '0.0')
-    end
     click_button 'Search'
 
     click_on 'More Info', match: :first
