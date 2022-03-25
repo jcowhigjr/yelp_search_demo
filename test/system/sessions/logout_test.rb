08c85a68dbfd74b997aec5865b9f1cc7c511b7ad
jcowhigjr <1895349+jcowhigjr@users.noreply.github.com>
2022-03-24 23:26:05 -0400

    added search icon to search attempted to fix the logout test by waiting for the results to load and the path redirect to complete removed the extra command in the github action ci run (#10)

--- test/system/sessions/logout_test.rb
+++ test/system/sessions/logout_test.rb
@@ -13,19 +13,19 @@ class LogoutTest < ApplicationSystemTestCase
   end
 
   test 'When I log out I can not leave a review' do
-
     visit '/login'
-
     fill_in 'email', with: @user.email
     click_on 'Log In'
     fill_in 'Password', with: default_password
     click_on 'Log In'
     click_on 'My Profile'
-    click_link 'Search'
+    click_link 'New Search'
+    assert_current_path '/searches/new'
     click_on 'Logout'
+    assert_current_path '/'
     fill_in 'search[query]', with: 'tacos'
-
     click_button 'Search'
+    assert_text 'Top Rated Searches for tacos near you'
     assert_current_path search_path(Search.last.id)
     click_on 'More Info', match: :first
     assert_text 'Login to add this shop to your favorites!'
