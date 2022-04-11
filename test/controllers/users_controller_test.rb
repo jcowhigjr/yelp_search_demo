59a45509bfcf8c6f4872461930e497f109842e71
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2022-04-11 17:43:30 -0400

    added user model tests for creating a valid user added some controller tests for current user create and show added some skipped controller tests for deleting a user added a controller test that shows the problem when a user is deleted skipped

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -20,4 +20,61 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to static_home_url
     assert_equal 'User Created!', flash[:success]
   end
+
+  test '#show' do
+    user_one = login(:one)
+    assert_equal 'Logged in!', user_one.flash[:success]
+  end
+
+  test '#destroy' do
+    skip 'not implemented'
+    user_one = login(:one)
+    assert_equal 'Logged in!', user_one.flash[:success]
+    assert_difference('User.count', -1) do
+      user_one.delete '/users/1'
+    end
+    assert_equal 'User 1 destroyed', user_one.flash[:notice]
+    assert_response :success
+    assert_equal '/login', user_one.current_path
+  end
+
+  test '#:show, bug missing user' do
+    user_one = login(:one)
+    assert_equal 'Logged in!', user_one.flash[:success]
+    User.find_by(id: user_one.session[:user_id]).destroy
+    skip 'this is the bug'
+    # ActiveRecord::RecordNotFound
+    # get '/users/1'
+  end
+
+
+  private
+
+  module CustomAssertions
+    def favorite_coffeeshop(coffeeshop)
+      # reference a named route, for maximum internal consistency!
+      post user_favorites_path, params: { coffeeshop_id: coffeeshop.id }, as: :turbo_stream
+      follow_redirect!
+    end
+
+    def unfavorite_coffeeshop(coffeeshop)
+      # binding.break unless coffeeshop
+      # reference a named route, for maximum internal consistency!
+      # https://github.com/hotwired/turbo-rails/blob/main/test/streams/streams_controller_test.rb#L38
+      delete user_favorite_path(id: coffeeshop.id), as: :turbo_stream
+      follow_redirect!
+    end
+  end
+
+  def login(who)
+    open_session do |sess|
+      sess.extend(CustomAssertions)
+      who = users(who)
+      get '/login'
+      assert_response :success
+
+      sess.post '/sessions', params: { email: who.email,
+                                       password: 'TerriblePassword' }
+    end
+  end
 end
