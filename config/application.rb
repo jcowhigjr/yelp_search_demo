987d6e63df6f58438336eacedfe8e2a3c043b842
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2022-07-05 10:07:46 -0400

    explicitly set when to use Rack:Deflator / Brotli
    https://github.com/romanbsd/heroku-deflater/issues/54#issuecomment-803400481

--- config/application.rb
+++ config/application.rb
@@ -34,18 +34,21 @@ module Jitter
     #   env.register_exporter %w[text/css application/javascript image/svg+xml], Sprockets::ExportersPack::BrotliExporter
     # end
 
+    # https://github.com/romanbsd/heroku-deflater/issues/54#issuecomment-803400481
     config.middleware.use Rack::Deflater,
-      include: Rack::Mime::MIME_TYPES.select{|k, v| v =~ /text|json|javascript/ }.values.uniq,
-      if: lambda {|env, status, headers, body| body.body.length > 512 }
+      include: Rack::Mime::MIME_TYPES.select{|_k, v| v =~ /text|json|javascript/ }.values.uniq,
+      if: lambda {|_env, _status, _headers, body| body.body.length > 512 }
 
     require 'rack/brotli'
 
     config.middleware.use Rack::Brotli,
-      include: Rack::Mime::MIME_TYPES.select{|k, v| v =~ /text|json|javascript/ }.values.uniq,
-      if: lambda {|env, status, headers, body| body.body.length > 512 },
+      include: Rack::Mime::MIME_TYPES.select{|_k, v| v =~ /text|json|javascript/ }.values.uniq,
+      if: lambda {|_env, _status, _headers, body| body.body.length > 512 },
       deflater: {
-        quality: 1
+        quality: 1,
       }
-
+      # https://github.com/denverstartupweek/dsw-site/pull/1104/files#diff-c1fd91cb1911a0512578b99f657554526f3e1421decdb9e908712beab57e10f9
+    # Deflate assets per https://www.schneems.com/2017/11/08/80-smaller-rails-footprint-with-rack-deflate/
+    config.middleware.insert_after ActionDispatch::Static, Rack::Deflater
   end
 end
