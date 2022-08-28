550ecf5e8a9b7065f9ec957e9815a955d25e8bf9
jcowhigjr <1895349+jcowhigjr@users.noreply.github.com>
2022-08-28 17:38:34 -0400

    adds validation for coffeshop#rating and test (#81)
    * adds validation for coffeshop#rating and test
    
    * fixed PR requests regarding range comments
    
    https://rubyreferences.github.io/rubyref/builtin/types/range.html

--- test/models/coffeeshop_test.rb
+++ test/models/coffeeshop_test.rb
@@ -4,4 +4,22 @@ class CoffeeshopTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  setup do
+    @coffeeshop = coffeeshops(:one)
+  end
+  test 'coffeeshop attributes must not be empty' do
+    @coffeeshop.rating = nil
+    assert_not @coffeeshop.valid?
+  end
+  test 'coffeeshop rating must be between 1 and 5' do
+    assert_predicate @coffeeshop, :valid?
+    @coffeeshop.rating = 0
+    assert_not @coffeeshop.valid?
+    @coffeeshop.rating = 5.5
+    assert_not @coffeeshop.valid?
+    @coffeeshop.rating = 1.5
+    assert_predicate @coffeeshop, :valid?
+    @coffeeshop.rating = 1
+    assert_predicate @coffeeshop, :valid?
+  end
 end
