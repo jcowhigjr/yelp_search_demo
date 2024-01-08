7c0fe437f78308fd453c0259c38a1e7a7b0f6851
jcowhigjr <jcowhigjr+dev@gmail.com>
2024-01-07 22:27:52 -0500

    try to fix concurrency issues

--- test/system/enabled_features_test.rb
+++ test/system/enabled_features_test.rb
@@ -1,33 +1,58 @@
 require 'application_system_test_case'
 
 class EnabledFeaturesTest < ApplicationSystemTestCase
+  # This is a workaround for a bug in Flipper
+  #
+  # https://github.com/flippercloud/flipper/issues/615#issuecomment-1879449641
 
-    # https://github.com/flippercloud/flipper/issues/615#issuecomment-1879449641
+  setup do
+    # Memory works here, as long as the same instance is shared across this thread and the app thread
+    @adapter = Flipper::Adapters::Memory.new
 
-    setup do
-        # Memory works here, as long as the same instance is shared across this thread and the app thread
-        @adapter = Flipper::Adapters::Memory.new
-
-        Flipper.configure do |config|
-         config.adapter { @adapter }
-        end
-        Flipper.instance = nil # Ensure flipper gets reset
+    Flipper.configure do |config|
+      config.adapter { @adapter }
     end
+    Flipper.instance = nil # Ensure flipper gets reset
+  end
 
-    test 'Flipper feature should be enabled' do
-      Flipper.add :early_access_preview
-      Flipper.enable :early_access_preview
+  test 'Flipper feature should be enabled' do
+    Flipper.add :early_access_preview
+    Flipper.enable :early_access_preview
 
-      assert_predicate Flipper[:early_access_preview], :enabled?
-    end
+    assert_predicate Flipper[:early_access_preview], :enabled?
+  end
 
-    test 'I should see Early Access Preview' do
-        Flipper.add :early_access_preview
-        Flipper.enable :early_access_preview
+  test 'I should see Early Access Preview' do
+    Flipper.add :early_access_preview
+    Flipper.enable :early_access_preview
 
-        visit '/'
+    visit '/'
 
-        assert_text 'Early Access Preview'
-    end
+    assert_text 'Early Access Preview'
+  end
 
+  test 'Flipper feature should be disabled' do
+    Flipper.add :early_access_preview
+    Flipper.disable :early_access_preview
+
+    assert_not_predicate Flipper[:early_access_preview], :enabled?
+  end
+
+  test 'I should not see Early Access' do
+
+    # Flipper.add :early_access_preview
+    Flipper.instance = nil # Ensure flipper gets reset
+
+    Flipper.disable :early_access_preview
+
+    visit '/'
+
+    assert_text 'Coming Soon: Sign up for Early Access'
+    refute_text 'Early Access Preview'
+  end
+
+  teardown do
+    Flipper.instance = nil # Ensure flipper gets reset
+
+  end
 end
