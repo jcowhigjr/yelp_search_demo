e574e1d265e6497d0c8e76d88dc8f87b726de3ae
jcowhigjr <jcowhigjr+dev@gmail.com>
2024-01-21 16:37:30 -0500

    simplified test and noted why it is flaky in parallel

--- config/initializers/flipper.rb
+++ config/initializers/flipper.rb
@@ -2,12 +2,16 @@
 
 require 'flipper/adapters/pstore'
 
-if ENV['FLIPPER_SPINNER_WHEEL']
-  Flipper.add :decision_wheel
-  Flipper.enable :decision_wheel
-end
-
-if ENV['FLIPPER_EARLY_ACCESS_PREVIEW']
+unless Rails.env.test?
+  if ENV['FLIPPER_SPINNER_WHEEL']
+    Flipper.add :decision_wheel
+    Flipper.enable :decision_wheel
+  end
+  # if ENV['FLIPPER_EARLY_ACCESS_PREVIEW']
   Flipper.add :early_access_preview
   Flipper.enable :early_access_preview
+  # end
 end
+
+# Rails.logger.debug do "Flipper features after running intializer: #{Flipper.features.map do |f|
+#  [f.key, f.enabled?] end.to_json}" end
--- test/system/enabled_features_test.rb
+++ test/system/enabled_features_test.rb
@@ -1,31 +1,34 @@
 require 'application_system_test_case'
 
+
 class EnabledFeaturesTest < ApplicationSystemTestCase
-  # This is a workaround for a bug in Flipper
-  #
+  # This is a workaround for Flipper
+  # This is flaky and fails periodically
   # https://github.com/flippercloud/flipper/issues/615#issuecomment-1879449641
-  # Resetting the Flipper instance in a setup block still failed when running tests on the second time
-  # moving it into the test itself seems to work
-    setup do
-      # Memory works here, as long as the same instance is shared across this thread and the app thread
-      @adapter = Flipper::Adapters::Memory.new
-      Flipper.configure do |config|
-        config.adapter { @adapter }
-      end
-      Flipper.instance = nil # Ensure flipper gets reset
-      Flipper.instance # Force flipper to be initialized
-    end
+  # https://github.com/flippercloud/flipper/pull/808/files/05e2ce04c2e68ffeafffc126bf5ced60f6b45fb1#r1457378780
 
-    # enable the feature manually in .env.test.local
-    # FLIPPER_EARLY_ACCESS_PREVIEW=true
-    test 'I should see Early Access Preview' do
-      Flipper[:early_access_preview].enable
+  setup do
+    # Memory works here, as long as the same instance is shared across this thread and the app thread
+    Flipper.instance.import(Flipper::Adapters::Memory.new)
+  end
 
-      assert_predicate Flipper[:early_access_preview], :enabled?
+  parallelize_setup do |_worker|
+    Flipper.instance=nil
+  end
 
-      visit '/'
+  # enable the feature manually in .env.test.local
+  # FLIPPER_EARLY_ACCESS_PREVIEW=true
+  test 'I should see Early Access Preview' do
+    Flipper.enable(:early_access_preview)
+    visit '/'
 
-      assert_text 'Early Access Preview'
-    end
+    assert_text 'Early Access Preview'
+  end
 
+  test 'I should not see Early Access Preview' do
+    Flipper.disable(:early_access_preview)
+    visit '/'
+
+    assert_text 'Coming Soon: Sign up for Early Access'
+  end
 end
