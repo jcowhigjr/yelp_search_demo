7e5cde7c885174dd75b1cf113f9048f7adc21343
jcowhigjr <jcowhigjr+dev@gmail.com>
2024-02-25 14:06:27 -0500

    Preparing to upgrade to Rails 7.1 (#472)
    - /bin/setup-next
    - next bundle exec rails app:update
    - # diffed the changes
    - # ran the tests
    - # wrapped the application and environment.rb failures to prevent new test failures
    - # tested rubocop and made fixes and exceptions based on this
    - # rails test will include test:prepare so removed references to it

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -9,7 +9,16 @@ Bundler.require(*Rails.groups)
 module Jitter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults  Rails::VERSION::STRING.to_f
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+
+    if Rails::VERSION::STRING >= '7.1'
+      config.action_controller.raise_on_missing_callback_actions = true
+      config.autoload_lib(ignore: %w[assets tasks])
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,8 +27,6 @@ module Jitter
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    # config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
 
     # Set permitted locales
     config.i18n.available_locales = [:en, :es, :fr, :"pt-BR"]
