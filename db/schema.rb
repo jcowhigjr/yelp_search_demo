4f00550a30a5ac2f627ecb7fb0f1df61fa343546
jcowhigjr <jcowhigjr+dev@gmail.com>
2024-06-15 14:59:16 -0400

    Feature/switch to rails 71 (#531)
    * Switch to using Rails 7.1
    
    Do some smoke testing.  Test suite has been passing for a while
    
    * updated rails to pull in security updates found on lefthook push
    
    * Security updates to next testing as well
    
    * bin/rails system:test --trace not available in rails 7.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_08_185633) do
+ActiveRecord::Schema[7.1].define(version: 2023_01_08_185633) do
   create_table "coffeeshops", force: :cascade do |t|
     t.string "name"
     t.string "address"
