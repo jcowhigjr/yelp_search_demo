610f41c21371a1bb44cf1982858a7733bae24292
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2025-11-17 16:44:33 -0500

    Ensure coffeeshop titles follow dark mode colors

---
+++ test/system/dark_mode_test.rb
@@ -0,0 +1,62 @@
+require 'application_system_test_case'
+
+class DarkModeTest < ApplicationSystemTestCase
+  test 'coffeeshop titles follow the theme text color variable' do
+    perform_search('coffee')
+
+    assert_selector '.card-title a', wait: 10
+
+    force_dark_mode!
+    set_theme_text('#123456')
+
+    card_color =
+      page.evaluate_script('window.getComputedStyle(document.querySelector(".card-title a")).color')
+
+    assert_equal 'rgb(18, 52, 86)', card_color
+
+    capture_search_results if capture_dark_mode?
+
+    click_more_info_safely
+
+    assert_selector '.page-name', wait: 10
+
+    set_theme_text('#123456')
+
+    page_name_color =
+      page.evaluate_script('window.getComputedStyle(document.querySelector(".page-name")).color')
+
+    assert_equal 'rgb(18, 52, 86)', page_name_color
+
+    capture_show_page if capture_dark_mode?
+  end
+
+  private
+
+  def capture_dark_mode?
+    ENV['CAPTURE_DARK_MODE'] == 'true'
+  end
+
+  def force_dark_mode!
+    page.execute_script(<<~JS)
+      const root = document.documentElement;
+      root.style.setProperty('--color-bg', '#18181b');
+      root.style.setProperty('--color-primary', '#223556');
+      root.style.setProperty('--color-primary-dark', '#16243a');
+      document.body.style.backgroundColor = '#18181b';
+    JS
+  end
+
+  def set_theme_text(color)
+    page.execute_script("document.documentElement.style.setProperty('--color-text', '#{color}')")
+  end
+
+  def capture_search_results
+    set_theme_text('#ffffff')
+    page.save_screenshot(Rails.root.join('tmp', 'dark-mode-search.png'), full: true)
+  end
+
+  def capture_show_page
+    set_theme_text('#ffffff')
+    page.save_screenshot(Rails.root.join('tmp', 'dark-mode-show.png'), full: true)
+  end
+end
