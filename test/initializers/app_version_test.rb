f92305de17a568859634459d235525fefbdb4474
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2026-01-07 17:04:43 -0500

    [ai] Changes for coder on issue #1044

--- test/initializers/app_version_test.rb
+++ test/initializers/app_version_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class AppVersionConfigTest < ActiveSupport::TestCase
   def test_app_version_matches_version_constant
-    assert_equal Jitter::VERSION, Rails.configuration.x.app_version
+    assert_equal Jitter::VERSION, Rails.configuration.x.app_version || nil
   end
 
   def test_app_version_looks_like_semantic_version
-    assert_match(/\A\d+\.\d+\.\d+\z/, Rails.configuration.x.app_version)
+    assert_match(/\A\d+\.\d+\.\d+\z/, Rails.configuration.x.app_version || nil)
   end
 
   def test_version_constant_matches_version_file
