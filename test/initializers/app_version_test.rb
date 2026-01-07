4ba83691d4215128f32bb10f4ec5bb6cbd57b3a6
John Cowhig <1895349+jcowhigjr@users.noreply.github.com>
2026-01-07 16:42:31 -0500

    [ai] Changes for coder on issue #1232

--- test/initializers/app_version_test.rb
+++ test/initializers/app_version_test.rb
@@ -16,4 +16,17 @@ class AppVersionConfigTest < ActiveSupport::TestCase
     version_file_content = File.read(version_file_path).strip
     assert_equal Jitter::VERSION, version_file_content, 'Jitter::VERSION should match VERSION file content'
   end
+
+  def test_app_version_is_increased_by_one_patch
+    previous_version = '0.2.28'.freeze
+    Rails.configuration.x.app_version = previous_version
+    
+    assert_equal '0.2.29', Rails.configuration.x.app_version, 'app_version is updated correctly'
+
+    old_value = Jitter::VERSION
+    Rails.configuration.x.app_version = old_value
+
+    assert_equal '0.2.29', Jitter::VERSION, "Jitter::VERSION is updated correctly"
+
+  end
 end
